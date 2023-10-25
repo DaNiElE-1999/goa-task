d1d89dc4ef60d55b3273c33432a0c66cc7e73729
Daniele Llazo <danielellazo@unyt.edu.al>
2023-10-25 17:23:42 +0200

    Corrected the connection properties, the connection string to be modified later using env file

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"database/sql"
 
 	"github.com/joho/godotenv"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	godotenv.Load(".env")
 
-	db, err := sql.Open("mysql", "root@/books")
+	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3333)/books") //toBeFixed with env var
 	if err != nil {
 		panic(err)
 	}
