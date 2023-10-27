7c1dcf5c0c7cf5a34f5ea24c0d9d8066dfedee4c
Daniele Llazo <danielellazo@unyt.edu.al>
2023-10-27 13:09:04 +0200

    Testing new way

--- books.go
+++ books.go
@@ -4,12 +4,34 @@ import (
 	books "books/gen/books"
 	"context"
 	"log"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 // books service example implementation.
 // The example methods log the requests and return zero values.
 type bookssrvc struct {
 	logger *log.Logger
+}
+
+func ExecuteQuery(query string) {
+	godotenv.Load(".env")
+
+	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3333)/books") //toBeFixed with env var
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
 }
 
 // NewBooks returns the books service implementation.
@@ -21,6 +43,12 @@ func NewBooks(logger *log.Logger) books.Service {
 func (s *bookssrvc) Create(ctx context.Context, p *books.Book) (res *books.Book, err error) {
 	res = &books.Book{}
 	s.logger.Print("books.create")
+	s.logger.Print("Received POST request with the following data:")
+	s.logger.Printf("ID: %d", *p.ID)
+	s.logger.Printf("Title: %s", *p.Title)
+	s.logger.Printf("Author: %s", *p.Author)
+	s.logger.Printf("BookCover: %s", *p.BookCover)
+	s.logger.Printf("PublishedAt: %s", *p.PublishedAt)
 	return
 }
 
