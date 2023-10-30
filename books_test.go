c97adcfb43598b5998ef98bad817d4aaffa500db
Daniele Llazo <danielellazo@unyt.edu.al>
2023-10-30 18:42:33 +0100

    Update test is still buggy

--- books_test.go
+++ books_test.go
@@ -216,3 +216,123 @@ func TestGetBook(t *testing.T) {
 		t.Errorf("Unexpected published date: got %v, want %v", *output.PublishedAt, *expectedOutput.PublishedAt)
 	}
 }
+
+func TestUpdateBook(t *testing.T) {
+	// Create a new mock database connection
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Failed to create mock database connection: %v", err)
+	}
+	defer db.Close()
+
+	// Create a new logger
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+
+	// Create a new books service with the mock database connection and logger
+	service := &bookssrvc{
+		logger: logger,
+		db:     db,
+	}
+
+	// Define the input and output for the test case
+	id := 13
+	title := "A Game of Thrones"
+	author := "George R. R. Martin"
+	bookCover := "https://www.example.com/a_game_of_thrones.jpg"
+	publishedAt := "1996-08-01"
+
+	input := &books.UpdateBookPayload{
+		ID: &id,
+		Book: &books.Book{
+			Author:      &author,
+			BookCover:   &bookCover,
+			PublishedAt: &publishedAt,
+			Title:       &title,
+		},
+	}
+
+	expectedOutput := &books.Book{
+		ID:          &id,
+		Title:       &title,
+		Author:      &author,
+		BookCover:   &bookCover,
+		PublishedAt: &publishedAt,
+	}
+
+	// Set up the mock database to expect the update query
+	mock.ExpectExec("^UPDATE books SET Title = ?, Author = ?, BookCover = ?, PublishedAt = ? WHERE Id = ?").
+		WithArgs(title, author, bookCover, publishedAt, id).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+
+	// Call the UpdateBook method on the books service
+	output, err := service.UpdateBook(context.Background(), input)
+
+	// Check for errors and unexpected output
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	if *output.ID != *expectedOutput.ID {
+		t.Errorf("Unexpected ID: got %v, want %v", output.ID, expectedOutput.ID)
+	}
+	if *output.Title != *expectedOutput.Title {
+		t.Errorf("Unexpected title: got %v, want %v", output.Title, expectedOutput.Title)
+	}
+	if *output.Author != *expectedOutput.Author {
+		t.Errorf("Unexpected author: got %v, want %v", output.Author, expectedOutput.Author)
+	}
+	if *output.BookCover != *expectedOutput.BookCover {
+		t.Errorf("Unexpected book cover: got %v, want %v", output.BookCover, expectedOutput.BookCover)
+	}
+	if *output.PublishedAt != *expectedOutput.PublishedAt {
+		t.Errorf("Unexpected published date: got %v, want %v", output.PublishedAt, expectedOutput.PublishedAt)
+	}
+
+	// Ensure all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %v", err)
+	}
+}
+
+func TestDeleteBook(t *testing.T) {
+	// Create a new mock database connection
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Failed to create mock database connection: %v", err)
+	}
+	defer db.Close()
+
+	// Create a new logger
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+
+	// Create a new books service with the mock database connection and logger
+	service := &bookssrvc{
+		logger: logger,
+		db:     db,
+	}
+
+	// Define the input and output for the test case
+	id := 1
+
+	input := &books.DeleteBookPayload{
+		ID: &id,
+	}
+
+	// Set up the mock database to expect the delete query
+	mock.ExpectExec("DELETE FROM books WHERE Id = ?").
+		WithArgs(*input.ID).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+
+	// Call the Delete method on the books service
+	err = service.DeleteBook(context.Background(), input)
+
+	// Check for errors
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+
+	// Ensure that all expectations were met
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Fatalf("Unfulfilled expectations: %v", err)
+	}
+}
